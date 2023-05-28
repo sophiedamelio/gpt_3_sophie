fef8e6b8884e26825037dce61da7368d0cecef2d
sophiedamelio <sophiedamelio@gmail.com>
2023-05-28 15:29:08 -0700

    first pass at navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -27,10 +27,14 @@ const Navbar = () => {
 					<Menu />
 				</div>
 			</div>
+			<div className='gpt3__navbar-sign'>
+				<p>Sign in</p>
+				<button type="button">Sign up</button>
+			</div>
 			<div className='gpt3__navbar-menu'>
 				{toggleMenu
-					? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}></RiCloseLine>
-					: <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}></RiMenu3Line>
+					? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+					: <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
 				}
 				{toggleMenu && (
 					<div className='gpt3__navbar-menu_container scale-up-center'>
