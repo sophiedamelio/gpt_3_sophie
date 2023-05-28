c07cf1ec85cb821e032fb08986265ce1d900bf46
sophiedamelio <sophiedamelio@gmail.com>
2023-05-28 14:30:13 -0700

    work on navbar, video at 1:02

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css';
+
+const Menu = () => (
+	<>
+		<p><a href="#home">Home</a></p>
+		<p><a href="#wgpt3">What is GPT3</a></p>
+		<p><a href="#possibility">Open AI</a></p>
+		<p><a href="#features">Case Studies</a></p>
+		<p><a href="#blog">Library</a></p>
+	</>
+)
 
 // BEM is naming convention for css classes
 
 const Navbar = () => {
+	const [toggleMenu, setToggleMenu] = useState(false);
 	return (
 		<div className='gpt3__navbar'>
 			<div className='gpt3__navbar-links'>
@@ -13,12 +24,25 @@ const Navbar = () => {
 					<img src={logo} alt="logo" />
 				</div>
 				<div className='gpt3__navbar-links_container'>
-					<p><a href="#home">Home</a></p>
-					<p><a href="#wgpt3">What is GPT3</a></p>
-					<p><a href="#possibility">Open AI</a></p>
-					<p><a href="#features">Case Studies</a></p>
-					<p><a href="#blog">Library</a></p>
+					<Menu />
 				</div>
+			</div>
+			<div className='gpt3__navbar-menu'>
+				{toggleMenu
+					? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)}></RiCloseLine>
+					: <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)}></RiMenu3Line>
+				}
+				{toggleMenu && (
+					<div className='gpt3__navbar-menu_container scale-up-center'>
+						<div className='gpt3__navbar-menu_container-links'>
+							<Menu />
+							<div className='gpt3__navbar-menu_conatiner-links-sign'>
+								<p>Sign in</p>
+								<button type="button">Sign up</button>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
 		</div>
 	)
